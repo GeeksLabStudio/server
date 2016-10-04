e54831deb942e241dbe9b844341708b76d42794e
n4bcak3 <n4bcak3@github.com>
2016-10-04 23:21:19 +0300

    resolving cors

--- server/core/express/index.js
+++ server/core/express/index.js
@@ -34,6 +34,17 @@ if (process.env.NODE_ENV === 'development') {
   }));
 }
 
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Accept");
+  res.header("Access-Control-Allow-Methods", 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Credentials', false);
+
+  next();
+})
+
 app.get('/status', expressStatusMonitor());
 
 app.use(compression());
